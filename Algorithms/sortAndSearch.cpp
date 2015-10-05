7d2a176ba16c259859c4930986e9c4803af482c2
Liam-Maloney <liam@liammaloney.ie>
2015-10-05 20:29:34 +0100

    Added file input to sortAndSearch.cpp

--- Algorithms/sortAndSearch.cpp
+++ Algorithms/sortAndSearch.cpp
@@ -1,49 +1,94 @@
 #include "stdafx.h"
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <string>
 
 //A Non Perfect implementation of Quick Sort
+//Takes a file from command line and sorts in memory
 //TODO: implement Binary Search
 
-template<typename T, int size>
-void swap(int right, int left, T(&numArr)[size])
+void swap(unsigned long right, unsigned long left, unsigned long numbers[])
 {
-	int temp = numArr[right];
-	numArr[right] = numArr[left];
-	numArr[left] = temp;
+	unsigned long temp = numbers[right];
+	numbers[right] = numbers[left];
+	numbers[left] = temp;
 }
 
-template<typename T, int size>
-void partitionStep(T (&numArr)[size], int left, int right)
+void partitionStep(unsigned long numbers[], unsigned long left, unsigned long right)
 {
-	//used as delimiters for the sets
-	int oldLeft = left;
-	int oldRight = right;
-	int pivot = numArr[(oldLeft + oldRight) / 2];
+	unsigned long oldLeft = left;
+	unsigned long oldRight = right;
+	unsigned long pivot = numbers[(oldLeft + oldRight) / 2l];
 	while (left != right)
 	{
-		while ((numArr[left] < pivot) && (left != right))
+		while ((numbers[left] < pivot) && (left != right))
 		{
 			left++;
 		}
 
-		while ((numArr[right] > pivot) && (left != right))
+		while ((numbers[right] > pivot) && (left != right))
 		{
 			right--;
 		}
 
 		if (left != right)
 		{
-			swap(right, left, numArr);
+			swap(right, left, numbers);
 		}
 	}
 
-	if (oldLeft != oldRight-1 && oldLeft != oldRight)
+	if (oldLeft != oldRight-1l && oldLeft != oldRight)
 	{
-		partitionStep(numArr, oldLeft, right);
+		partitionStep(numbers, oldLeft, right);
 
 	}
 
-	if (oldLeft + 1 != oldRight && oldLeft != oldRight)
+	if (oldLeft + 1l != oldRight && oldLeft != oldRight)
 	{
-		partitionStep(numArr, right, oldRight);
+		partitionStep(numbers, right, oldRight);
 	}
+}
+
+void loadFile(unsigned long unsortedArr[], std::string source)
+{
+	std::fstream data;
+	data.open(source);
+	unsigned long i = 0;
+	unsigned long dataCurrent = 0;
+	while (!data.eof())
+	{
+		data >> dataCurrent;
+		unsortedArr[i] = dataCurrent;
+		i++;
+	}
+}
+
+unsigned long determinSize(std::string sourceName)
+{
+	std::fstream data;
+	data.open(sourceName);
+	unsigned long count = 0;
+	std::string current;
+	while (data >> current)
+	{
+		count++;
+		std::cout << count << std::endl;
+	}
+	return count;
+}
+
+int main(int argc, char *argv[])
+{
+	const int SOURCE_FILE = 1;
+	std::string sourceName;
+	sourceName = argv[SOURCE_FILE];
+	std::cout << "Counting Elements..." << std::endl;
+	unsigned long size = determinSize(sourceName);
+	unsigned long* numArr = new unsigned long[size];
+	loadFile(numArr, sourceName);
+	std::cout << "Begin Sorting..." << std::endl;
+	partitionStep(numArr, 0, size - 1);
+	std::cout << "Sorted" << std::endl;
+	delete [] numArr;
 }
